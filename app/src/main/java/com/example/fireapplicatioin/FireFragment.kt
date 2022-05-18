b1d121173e2886552ecf8a2197f983129fa179d7
ShuLgaSeR-collab <shulgin.sery@yandex.ru>
2022-05-18 21:15:13 +0500

    Near Services

--- app/src/main/java/com/example/fireapplicatioin/FireFragment.kt
+++ app/src/main/java/com/example/fireapplicatioin/FireFragment.kt
@@ -2,37 +2,90 @@ package com.example.fireapplicatioin
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.location.Location
+import android.os.AsyncTask
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import com.example.fireapplicatioin.data.Datasource
 import com.example.fireapplicatioin.databinding.FragmentFireBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.*
+import com.google.android.gms.tasks.Task
+import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.lang.Exception
+import java.lang.StringBuilder
+import java.net.HttpURLConnection
+import java.net.URL
 import java.util.*
+import kotlin.collections.HashMap
 
 class FireFragment : Fragment(), OnMapReadyCallback {
-    private lateinit var map: GoogleMap
+    lateinit var map: GoogleMap
     private val REQUEST_LOCATION_PERMISSION = 1
+
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+
+    var currentLat: Double = 0.0
+    var currentLong: Double = 0.0
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         val binding = DataBindingUtil.inflate<FragmentFireBinding>(inflater, R.layout.fragment_fire, container, false)
         val mapFragment = childFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        val placeTypeList = arrayListOf("hospital", "fire", "fire_department", "atm")
+        val placeNameList = arrayListOf("Hospital", "Fire", "Fire Department", "ATM")
+
+        binding.spType?.adapter = ArrayAdapter(context!!, android.R.layout.simple_spinner_dropdown_item
+        , placeNameList)
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(context!!)
+
+        binding.findButton?.setOnClickListener{
+            val i = binding.spType?.selectedItemPosition
+            val url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json" +
+                    "?location=" + currentLat + "," + currentLong +
+                    "&radius=5000" +
+                    "&type=" + placeTypeList[i!!] +
+                    "&sensor=true" +
+                    "&key=" + getResources().getString(R.string.google_maps_key)
+            PlaceTask(map).execute(url)
+        }
+
+
         return binding.root
+    }
+
+    fun getCurrentLocation(map: GoogleMap){
+        if(isPermissionGranted()){
+            var task: Task<Location> = fusedLocationProviderClient.lastLocation
+            task.addOnSuccessListener { location: Location ->
+                currentLong = location.latitude
+                currentLat = location.longitude
+                map.animateCamera(CameraUpdateFactory.newLatLngZoom(
+                    LatLng(currentLat, currentLong), 10f
+                ))
+            }
+        }
     }
 
     private fun isPermissionGranted() : Boolean {
@@ -125,6 +178,70 @@ class FireFragment : Fragment(), OnMapReadyCallback {
             )
             poiMarker?.showInfoWindow()
         }
+    }
+
+    private class PlaceTask(val map: GoogleMap): AsyncTask<String, Int, String>(){
+        override fun doInBackground(vararg p0: String?): String {
+            val data = downloadUrl(p0[0]!!)
+            return data
+        }
+
+        override fun onPostExecute(result: String?) {
+            ParserTask(map).execute(result)
+        }
+        private fun downloadUrl(string: String): String{
+            val url = URL(string)
+            val connection = url.openConnection() as HttpURLConnection
+
+            connection.connect()
+
+            val inputStream = connection.inputStream
+            val reader = BufferedReader(InputStreamReader(inputStream))
+            val builder = StringBuilder()
+            var line = ""
+            reader.lineSequence().forEach {
+                builder.append(it)
+            }
+            val data = builder.toString()
+
+            reader.close()
+            return data
+        }
+
+        private class ParserTask(val map: GoogleMap): AsyncTask<String, Int, List<HashMap<String, String>>>() {
+            override fun doInBackground(vararg p0: String?): List<HashMap<String, String>> {
+                val jsonParser = JsonParser()
+                var mapList: List<HashMap<String, String>>? = null
+                val obj: JSONObject?
+                try {
+                    obj = JSONObject(p0[0]!!)
+                    mapList = jsonParser.parseResult(obj)
+                }catch (e: Exception){
+
+                }
+                return mapList!!
+            }
+
+            override fun onPostExecute(result: List<HashMap<String, String>>?) {
+                map.clear()
+                val size = result!!.size - 1
+                for(i in 0..size){
+                    val hashMapList = result[i]
+                    val lat = hashMapList["lat"]!!.toDouble()
+                    val lng = hashMapList["lng"]!!.toDouble()
+                    val name = hashMapList["name"]
+                    val latLng = LatLng(lat, lng)
+                    val markerOptions = MarkerOptions()
+
+                    markerOptions.position(latLng)
+                    markerOptions.title(name)
+
+                    map.addMarker(markerOptions)
+                }
+            }
+
+        }
+
     }
 
 }
---
+++ app/src/main/java/com/example/fireapplicatioin/JsonParser.kt
@@ -0,0 +1,51 @@
+package com.example.fireapplicatioin
+
+import org.json.JSONArray
+import org.json.JSONObject
+import java.lang.Exception
+
+public class JsonParser {
+    fun parseJsonObject(obj: JSONObject): HashMap<String, String>{
+        var dataList = HashMap<String, String>()
+        try {
+            val name = obj.getString("name")
+            val latitude = obj.getJSONObject("geometry")
+                .getJSONObject("location")
+                .getString("lat")
+            val longitude = obj.getJSONObject("geometry")
+                .getJSONObject("location")
+                .getString("lng")
+
+            dataList.put("name", name)
+            dataList.put("lat", latitude)
+            dataList.put("lng", longitude)
+        }catch(e: Exception){
+
+        }
+
+        return dataList
+    }
+
+    fun parseJsonArray(objects: JSONArray): List<HashMap<String, String>>{
+        var dataList = ArrayList<HashMap<String, String>>()
+        for (i in 0..objects.length()){
+            try {
+                val data = parseJsonObject(objects.get(i) as JSONObject)
+                dataList.add(data)
+            }catch (e: Exception){
+
+            }
+        }
+        return dataList
+    }
+
+    fun parseResult(obj: JSONObject): List<HashMap<String, String>>{
+        var jsonArray: JSONArray? = null
+        try {
+            jsonArray = obj.getJSONArray("results")
+        }catch (e: Exception){
+
+        }
+        return parseJsonArray(jsonArray!!)
+    }
+}
